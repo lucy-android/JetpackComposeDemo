b24d5fceb0c59abf9b38d701ddebaff8f5d78802
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2023-12-26 17:42:27 +0300

    JET-2: show indeterminate progressbar on screen 2

--- app/src/main/java/com/example/jetpack/compose/demo/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/compose/demo/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
@@ -22,6 +23,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -417,9 +419,11 @@ fun SecondScreen(
                     text = stringResource(id = R.string.send_another_code), spanStyle = spanStyles3
                 )
             )
+            CircularProgressIndicator(
+                modifier = Modifier.width(64.dp),
+                color = MaterialTheme.colorScheme.secondary
+            )
         }
-
-
 
         Row(modifier = Modifier.weight(1f)) {
             Box(
